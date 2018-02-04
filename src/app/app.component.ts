8bc1d4fb1588674fca94a40a3c40c5a14abf0d8c
Borja Sahuquillo <borja.sahuquillo@gnommo.com>
2018-02-04 15:01:55 +0100

    FIRST COMMIT

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+declare var device;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'app';
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    document.addEventListener("deviceready", function() {
+      alert(device.platform);
+    }, false);
+  }
+}
